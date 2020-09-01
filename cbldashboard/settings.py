5baaf928ca1423a31bda4958baedde20a0fbd6e7
Tiago Cordeiro <tiago@mulhergorila.com>
2020-09-01 16:58:13 -0300

    HotFix AWS storage

--- cbldashboard/settings.py
+++ cbldashboard/settings.py
@@ -184,7 +184,7 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
     }
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
-    AWS_QUERYSTRING_AUTH = False
+    AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
 
     AWS_DEFAULT_ACL = "public-read"
